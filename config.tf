ef9da93ac9ce5b61517d6b02e1dedb6e99ebf27a
Andrew A <tov_mayor@mail.ru>
2022-09-11 11:57:10 +0300

    4 -> 2

--- config.tf
+++ config.tf
@@ -39,8 +39,8 @@ resource "yandex_compute_instance" "build" {
   allow_stopping_for_update = true
 
   resources {
-    cores  = 4
-    memory = 4
+    cores  = 2
+    memory = 2
     core_fraction = 20  
   }
 
